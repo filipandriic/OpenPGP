c112104e969a54e45008c5d9a614322412cf892a
Filip Andric <andricfilip2000@gmail.com>
2022-06-08 22:17:00 +0200

    pls

---
+++ src/etf/openpgp/af18273dij18203d/back/SendController.java
@@ -0,0 +1,48 @@
+package etf.openpgp.af18273dij18203d.back;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.openpgp.PGPPublicKeyRing;
+import org.bouncycastle.openpgp.PGPPublicKeyRingCollection;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
+
+public class SendController {
+
+	//3des with ede conf
+	//aes 128
+	
+	public static void send(boolean toEncrypt, boolean toSign, boolean toCompress, boolean toRadix, PGPSecretKeyRing secretKeyRing, PGPPublicKeyRingCollection publicKeyRingCollection, int algorithm, String password) {
+		OutputStream message = null;
+		try {
+			message = new FileOutputStream(new File("encrypted_message.gpg"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		// Signing
+		
+
+		// Compression
+		
+
+		// Encryption
+		
+		
+		// Radix64
+		if (toRadix) message = new ArmoredOutputStream(message);
+		
+		
+		
+	}
+	
+	private static void compress() {
+		
+	}
+	
+}
