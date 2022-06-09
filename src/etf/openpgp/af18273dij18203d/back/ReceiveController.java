8e37cbbf254c60646d206390776cf01b1e8aec43
Filip Andric <andricfilip2000@gmail.com>
2022-06-09 18:13:17 +0200

    ne znam, ne mogu vise

--- src/etf/openpgp/af18273dij18203d/back/ReceiveController.java
+++ src/etf/openpgp/af18273dij18203d/back/ReceiveController.java
@@ -63,165 +63,166 @@ public class ReceiveController {
 			this.info.setRadixStatus(false);
 		}
 
-			JcaPGPObjectFactory pgpFact = new JcaPGPObjectFactory(in);
-			PGPEncryptedDataList enc;
-			PGPPrivateKey sKey = null;
-			PGPPublicKeyEncryptedData pbe = null;
-			Object o = pgpFact.nextObject();
+		JcaPGPObjectFactory pgpFact = new JcaPGPObjectFactory(in);
+		PGPEncryptedDataList enc;
+		PGPPrivateKey sKey = null;
+		PGPPublicKeyEncryptedData pbe = null;
+		Object o = pgpFact.nextObject();
 
-			this.info.setDecryptionStatus(false);
-			// PGP Marker check
+		this.info.setDecryptionStatus(false);
+		// PGP Marker check
 
-			if (o instanceof PGPMarker) {
-				o = pgpFact.nextObject();
-			}
-			// Decryption check
-			else if (o instanceof PGPEncryptedDataList) {
-				this.info.setDecryptionStatus(true);
-				enc = (PGPEncryptedDataList) o;
-				Iterator<PGPEncryptedData> it = enc.getEncryptedDataObjects();
+		if (o instanceof PGPMarker) {
+			o = pgpFact.nextObject();
+		}
+		// Decryption check
+		else if (o instanceof PGPEncryptedDataList) {
+			this.info.setDecryptionStatus(true);
+			enc = (PGPEncryptedDataList) o;
+			Iterator<PGPEncryptedData> it = enc.getEncryptedDataObjects();
 
-				while (sKey == null && it.hasNext()) {
-					pbe = (PGPPublicKeyEncryptedData) it.next();
+			while (sKey == null && it.hasNext()) {
+				pbe = (PGPPublicKeyEncryptedData) it.next();
 
-					PGPSecretKey pgpSecKey = ManageKeysController.getSecretKeyRing(pbe.getKeyID()).getSecretKey();
+				PGPSecretKey pgpSecKey = ManageKeysController.getSecretKeyRing(pbe.getKeyID()).getSecretKey();
 
-					if (pgpSecKey != null) {
-						this.info.setPrivatekey(new KeyInfoWrapper(pgpSecKey.getUserIDs().next(), "", pgpSecKey.getKeyID()));
-						try {
-							sKey = pgpSecKey.extractPrivateKey(
-									new JcePBESecretKeyDecryptorBuilder().setProvider("BC").build(password.toCharArray()));
-						} catch (PGPException e) {
-							System.out.println("Wrong password.");
-							//Window.showError("Wrong pass");
-							return false;
-						}
+				if (pgpSecKey != null) {
+					this.info.setPrivatekey(new KeyInfoWrapper(pgpSecKey.getUserIDs().next(), "", pgpSecKey.getKeyID()));
+					try {
+						sKey = pgpSecKey.extractPrivateKey(
+								new JcePBESecretKeyDecryptorBuilder().setProvider("BC").build(password.toCharArray()));
+					} catch (PGPException e) {
+						System.out.println("Wrong password.");
+						//Window.showError("Wrong pass");
+						return false;
 					}
-					
 				}
 				
+			}
+			
 //				if (sKey == null) {
 //					throw new IllegalArgumentException("Secret key for message not found.");
 //				}
-				
-				InputStream decryptedStream = null;
-				try {
-					decryptedStream = pbe
-							.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(sKey));
-				} catch (PGPException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				pgpFact = new JcaPGPObjectFactory(decryptedStream);
-
-				o = pgpFact.nextObject();
-
-				this.info.setDecryptionSuccess(true);
-			}
-			// Compression check
-			this.info.setCompressionStatus(false);
-			if (o instanceof PGPCompressedData) {
-				this.info.setCompressionStatus(true);
-				PGPCompressedData cData = (PGPCompressedData) o;
-				try {
-					pgpFact = new JcaPGPObjectFactory(cData.getDataStream());
-				} catch (PGPException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				o = pgpFact.nextObject();
-				this.info.setCompressionSuccess(true);
+			
+			InputStream decryptedStream = null;
+			try {
+				decryptedStream = pbe
+						.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(sKey));
+			} catch (PGPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 
-			// Signature check
+			pgpFact = new JcaPGPObjectFactory(decryptedStream);
 
-			if (o instanceof PGPOnePassSignatureList) {
+			o = pgpFact.nextObject();
 
-				this.info.setVerificationStatus(true);
-				PGPOnePassSignatureList lsig = (PGPOnePassSignatureList) o;
+			this.info.setDecryptionSuccess(true);
+		}
+		// Compression check
+		this.info.setCompressionStatus(false);
+		if (o instanceof PGPCompressedData) {
+			this.info.setCompressionStatus(true);
+			PGPCompressedData cData = (PGPCompressedData) o;
+			try {
+				pgpFact = new JcaPGPObjectFactory(cData.getDataStream());
+			} catch (PGPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			o = pgpFact.nextObject();
+			this.info.setCompressionSuccess(true);
+		}
 
-				PGPOnePassSignature sig = lsig.get(0);
+		// Signature check
 
-				PGPPublicKeyRingCollection pgpRing = null;
-				PGPPublicKey key = null;
-				try {
-					pgpRing = new PGPPublicKeyRingCollection(
-							PGPUtil.getDecoderStream(new FileInputStream(publicKeys)), new JcaKeyFingerprintCalculator());
+		if (o instanceof PGPOnePassSignatureList) {
 
-					key = pgpRing.getPublicKey(sig.getKeyID());
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (PGPException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			this.info.setVerificationStatus(true);
+			PGPOnePassSignatureList lsig = (PGPOnePassSignatureList) o;
+
+			PGPOnePassSignature sig = lsig.get(0);
+
+			PGPPublicKeyRingCollection pgpRing = null;
+			PGPPublicKey key = null;
+			try {
+				pgpRing = new PGPPublicKeyRingCollection(
+						PGPUtil.getDecoderStream(new FileInputStream(publicKeys)), new JcaKeyFingerprintCalculator());
+
+				key = pgpRing.getPublicKey(sig.getKeyID());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (PGPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+			String[] identity = key.getUserIDs().next().split(" ");
+			this.info.setPublickey(new KeyInfoWrapper(identity[0], identity[1], key.getKeyID()));
+			
+			try {
+				sig.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), key);
+			} catch (PGPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			PGPLiteralData ld = (PGPLiteralData) pgpFact.nextObject();
+
+			InputStream dIn = ld.getInputStream();
+			File tmp = new File(ld.getFileName());
+
+			int ch;
+			FileOutputStream out = new FileOutputStream(tmp);
+
+			while ((ch = dIn.read()) >= 0) {
+				sig.update((byte) ch);
+				out.write(ch);
+			}
+			
+
+			this.info.setDecryptedFile(tmp);
+			PGPSignatureList vsig = (PGPSignatureList) pgpFact.nextObject();
+
+			out.close();
+			try {
+				if (sig.verify(vsig.get(0))) {
+					this.info.setVerificationSuccess(true);
+					System.out.println("Signature verified.");
+
+				} else {
+					this.info.setVerificationSuccess(false);
+					System.err.println("Signature verification failed.");
 				}
+			} catch (PGPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if (o instanceof PGPLiteralData) {
+			this.info.setVerificationStatus(false);
+			PGPLiteralData ld = (PGPLiteralData) o;
 
-				
-				String[] identity = key.getUserIDs().next().split(" ");
-				this.info.setPublickey(new KeyInfoWrapper(identity[0], identity[1], key.getKeyID()));
-				
-				try {
-					sig.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), key);
-				} catch (PGPException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				PGPLiteralData ld = (PGPLiteralData) pgpFact.nextObject();
-
-				InputStream dIn = ld.getInputStream();
-				File tmp = new File(ld.getFileName());
-
-				int ch;
-				FileOutputStream out = new FileOutputStream(tmp);
-
-				while ((ch = dIn.read()) >= 0) {
-					sig.update((byte) ch);
-					out.write(ch);
-				}
-
-				out.close();
-				this.info.setDecryptedFile(tmp);
-				PGPSignatureList vsig = (PGPSignatureList) pgpFact.nextObject();
-
-				try {
-					if (sig.verify(vsig.get(0))) {
-						this.info.setVerificationSuccess(true);
-						System.out.println("Signature verified.");
-
-					} else {
-						this.info.setVerificationSuccess(false);
-						System.err.println("Signature verification failed.");
-					}
-				} catch (PGPException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} else if (o instanceof PGPLiteralData) {
-				this.info.setVerificationStatus(false);
-				PGPLiteralData ld = (PGPLiteralData) o;
-
-				String outFileName = ld.getFileName();
-				if (outFileName.length() == 0) {
-					outFileName = this.file.getName();
-				}
-				File tmp = new File(outFileName);
-				InputStream dIn = ld.getInputStream();
-				OutputStream out = new FileOutputStream(outFileName);
+			String outFileName = ld.getFileName();
+			if (outFileName.length() == 0) {
+				outFileName = this.file.getName();
+			}
+			File tmp = new File(outFileName);
+			InputStream dIn = ld.getInputStream();
+			OutputStream out = new FileOutputStream(outFileName);
 
 //				Streams.pipeAll(dIn, out, 8192);
 
-				out.close();
+			out.close();
 
-				this.info.setDecryptedFile(tmp);
-			}
+			this.info.setDecryptedFile(tmp);
+		}
 
-			this.info.setIntegritySuccess(true);
+		this.info.setIntegritySuccess(true);
 //			if (pbe != null && pbe.isIntegrityProtected()) {
 //				this.info.setIntegrityStatus(true);
 //				if (!pbe.verify()) {
